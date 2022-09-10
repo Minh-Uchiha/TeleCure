28d0c98487f69eee7c8e08d5075993e07817ea17
Minh Uchiha <minhsalamander09@gmail.com>
2022-09-10 11:48:19 +0700

    Adding main components and pages

---
+++ front-end/src/pages/index.js
@@ -0,0 +1,5 @@
+export { default as ChatRoomPage } from "./ChatRoomPage";
+export { default as CheckoutPage } from "./CheckoutPage";
+export { default as WelcomePage } from "./WelcomePage";
+export { default as DoctorPage } from "./DoctorPage";
+export { default as PatientPage } from "./PatientPage";
