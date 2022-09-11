272b63f19d3f2dad1ccdcabdedea9b680c9f45ea
Nokeen <cordrepayne@gmail.com>
2022-09-10 22:31:07 -0500

    Patients can now create accounts and log in

--- front-end/src/pages/PatientSignupPage.jsx
+++ front-end/src/pages/PatientSignupPage.jsx
@@ -1,17 +1,27 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from 'axios';
 import "../css/SignupPage.css";
+import { AuthContext } from '../context/auth.context';
+
+const URL = process.env.REACT_APP_API_URL;
 
 const SignupForm = () => {
   const [isSignUp, setIsSignUp] = useState(true);
   const [error, setError] = useState("");
+  const navigate = useNavigate();
+  const { storeToken, isLoggedIn, authenticateUser } = useContext(AuthContext);
   const [user, setUser] = useState({
     foreName: "",
     surName: "",
-    phonenumber: "",
+    title: "",
+    email_address: "",
+    phone_number: "",
+    job_title: "",
+    age: "",
+    dob: "",
     password: "",
     reenteredPassword: "",
-    email: "",
   });
 
   const handleChange = (e) => {
@@ -20,101 +30,180 @@ const SignupForm = () => {
     });
   };
 
-  const handleSubmit = () => {};
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-  return (
-    <section className="sign-up-form">
-      <p className="error">{error}</p>
-      <div
-        style={{ display: `${isSignUp ? "block" : "none"}` }}
-        className="input-field"
-      >
-        <input
-          type="text"
-          name="foreName"
-          placeholder="First name"
-          value={user.foreName}
-          onChange={handleChange}
-        />
-      </div>
-      <div
-        style={{ display: `${isSignUp ? "block" : "none"}` }}
-        className="input-field"
-      >
-        <input
-          type="text"
-          name="surName"
-          placeholder="Last name"
-          value={user.surName}
-          onChange={handleChange}
-        />
-      </div>
-      <div className="input-field">
-        <input
-          type="text"
-          name="email"
-          placeholder="Email"
-          value={user.email}
-          onChange={handleChange}
-        />
-      </div>
-      <div
-        style={{ display: `${isSignUp ? "block" : "none"}` }}
-        className="input-field"
-      >
-        <input
-          type="text"
-          name="phonenumber"
-          placeholder="Phone number"
-          value={user.phonenumber}
-          onChange={handleChange}
-        />
-      </div>
-      <div className="input-field">
-        <input
-          type="password"
-          name="password"
-          placeholder="Password"
-          value={user.password}
-          onChange={handleChange}
-        />
-      </div>
-      <div
-        style={{ display: `${isSignUp ? "block" : "none"}` }}
-        className="input-field"
-      >
-        <input
-          type="password"
-          name="reenteredPassword"
-          placeholder="Re-enter password"
-          value={user.reenteredPassword}
-          onChange={handleChange}
-        />
-      </div>
-      <div className="submit-field">
-        <input
-          type="button"
-          value={`${isSignUp ? "Sign up" : "Sign in"}`}
-          onClick={handleSubmit}
-        />
-      </div>
-      <p>
-        {isSignUp ? `Already have an account?` : "Don't have an account?"}{" "}
-        <span onClick={() => setIsSignUp((oldIsSignUp) => !oldIsSignUp)}>
-          {isSignUp ? "Sign in" : "Sign up"}
-        </span>
-      </p>
-    </section>
-  );
-};
+    axios.post(`${URL}/auth/${isSignUp ? 'patientSignup' : 'patientLogin'}`, user)
+    .then(response => {
+      if(!isSignUp){
+        storeToken(response.data.authToken)
+        authenticateUser();
+      }
+      navigate('/');
+    })
+    .catch((error) => {
+      const errorDescription = error.response.data.message;
+      setError(errorDescription);
+    })
+  };
+  if(!isLoggedIn){
+    return (
+      <section className="sign-up-form">
+        <p className="error">{error}</p>
+        <div
+          style={{ display: `${isSignUp ? "block" : "none"}` }}
+          className="input-field"
+        >
+          <input
+            type="text"
+            name="foreName"
+            placeholder="First name"
+            value={user.foreName}
+            onChange={handleChange}
+          />
+        </div>
+        <div
+          style={{ display: `${isSignUp ? "block" : "none"}` }}
+          className="input-field"
+        >
+          <input
+            type="text"
+            name="surName"
+            placeholder="Last name"
+            value={user.surName}
+            onChange={handleChange}
+          />
+        </div>
+        <div
+          style={{ display: `${isSignUp ? "block" : "none"}` }}
+          className="input-field"
+        >
+          <input
+            type="text"
+            name="title"
+            placeholder="Mr./Ms./Mrs."
+            value={user.title}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="input-field">
+          <input
+            type="text"
+            name="email_address"
+            placeholder="Email"
+            value={user.email_address}
+            onChange={handleChange}
+          />
+        </div>
+        <div
+          style={{ display: `${isSignUp ? "block" : "none"}` }}
+          className="input-field"
+        >
+          <input
+            type="text"
+            name="phone_number"
+            placeholder="Phone number"
+            value={user.phone_number}
+            onChange={handleChange}
+          />
+        </div>
+        <div
+          style={{ display: `${isSignUp ? "block" : "none"}` }}
+          className="input-field"
+        >
+          <input
+            type="text"
+            name="job_title"
+            placeholder="*Job Title"
+            value={user.job_title}
+            onChange={handleChange}
+          />
+        </div>
+        <div
+          style={{ display: `${isSignUp ? "block" : "none"}` }}
+          className="input-field"
+        >
+          <input
+            type="text"
+            name="age"
+            placeholder="Age"
+            value={user.age}
+            onChange={handleChange}
+          />
+        </div>
+        <div
+          style={{ display: `${isSignUp ? "block" : "none"}` }}
+          className="input-field"
+        >
+          <input
+            type="text"
+            name="dob"
+            placeholder="Date of Birth"
+            value={user.dob}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="input-field">
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={user.password}
+            onChange={handleChange}
+          />
+        </div>
+        <div
+          style={{ display: `${isSignUp ? "block" : "none"}` }}
+          className="input-field"
+        >
+          <input
+            type="password"
+            name="reenteredPassword"
+            placeholder="Re-enter password"
+            value={user.reenteredPassword}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="submit-field">
+          <input
+            type="button"
+            value={`${isSignUp ? "Sign up" : "Sign in"}`}
+            onClick={handleSubmit}
+          />
+        </div>
+        <p>
+          {isSignUp ? `Already have an account?` : "Don't have an account?"}{" "}
+          <span onClick={() => setIsSignUp((oldIsSignUp) => !oldIsSignUp)}>
+            {isSignUp ? "Sign in" : "Sign up"}
+          </span>
+        </p>
+      </section>
+    );
+  }else if(isLoggedIn){
+    return (
+      <form className="sign-up-form">
+        <p>Already logged in!</p>
+      </form>
+    )
+  }
+}
 
 const PatientSignupPage = () => {
+  const { isLoggedIn } = useContext(AuthContext);
   return (
     <div className="signup-form-container">
+    {isLoggedIn &&
       <h1 style={{ color: "#635bff" }}>
-        Sign up to get advices from our world-class doctors!
+        Welcome back!
       </h1>
-      <SignupForm />
+    }{!isLoggedIn &&
+      <>
+        <h1 style={{ color: "#635bff" }}>
+          Sign up to get advices from our world-class doctors!
+        </h1>
+        <SignupForm />
+      </>
+    }
     </div>
   );
 };
