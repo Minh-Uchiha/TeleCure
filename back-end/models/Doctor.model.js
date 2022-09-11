0899348590fcd228ae89fd93ae9c5f28d67206ce
Nokeen <cordrepayne@gmail.com>
2022-09-10 19:28:01 -0500

    Created full models for chatlog, messages, patients, and doctors

---
+++ back-end/models/Doctor.model.js
@@ -0,0 +1,76 @@
+const { Schema, model } = require("mongoose");
+
+const doctorSchema = new Schema(
+  {
+    dr_forename: {
+        type: String,
+        required: true
+    },
+    dr_surname: {
+        type: String,
+        required: true
+      },
+    title: {
+        type: String,
+        required: true
+    },
+    specialty: {
+        type: String,
+        required: true
+    },
+    hospital: {
+        type: String,
+        required: true
+    },
+    years_expirience: {
+        type: Number,
+        required: true
+    },
+    phone_number: {
+        type: Number,
+        required: true
+    },
+    email_address: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    avatar: {
+        type: String,
+        default: '',
+    },
+    available: {
+        type: Boolean,
+        default: true,
+    },
+    average_ratings: {
+        type: Number,
+        default: 0,
+    },
+    patients_number: {
+        type: Number,
+        default: 0,
+    },
+    earnings: {
+        type: Number,
+        default: 0,
+    },
+    net_earnings: {
+        type: Number,
+        default: 0
+    },
+    chat_log: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Message'
+    }]
+    },
+    {
+        timestamps: true,
+    }
+);
+
+module.exports = model("Doctor", doctorSchema);
